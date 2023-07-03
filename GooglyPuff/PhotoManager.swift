bf6cd11bad9970b7de92592972d99e10ad538d59
Icung <saiful-islam@outlook.co.id>
2023-07-03 12:47:17 +0700

    Dispatch Barriers

--- GooglyPuff/PhotoManager.swift
+++ GooglyPuff/PhotoManager.swift
@@ -52,16 +52,37 @@ final class PhotoManager {
   private init() {}
   static let shared = PhotoManager()
 
+  private let concurrentPhotoQueue = DispatchQueue(
+    label: "com.raywenderlich.GooglyPuff.photoQueue",
+    attributes: .concurrent
+  )
   private var unsafePhotos: [Photo] = []
 
   var photos: [Photo] {
-    return unsafePhotos
+    var photosCopy: [Photo] = []
+    concurrentPhotoQueue.sync {
+      photosCopy = self.unsafePhotos
+    }
+      
+    return photosCopy
   }
 
   func addPhoto(_ photo: Photo) {
     unsafePhotos.append(photo)
-    DispatchQueue.main.async { [weak self] in
-      self?.postContentAddedNotification()
+    concurrentPhotoQueue.async(flags: .barrier) { [weak self] in
+        guard let self = self else {
+            return
+        }
+        
+        self.unsafePhotos.append(photo)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            self.postContentAddedNotification()
+        }
     }
   }
 
